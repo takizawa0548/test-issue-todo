0d6d8d656577068ac4a5217c0b2414876fe55219
mr_noppo <takizawa20383461@gmail.com>
2024-10-02 20:24:30 +0900

    TODO　コメント

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
+          {/* TODO hello_worldの実装が必要 */}
           <li>Save and see your changes instantly.</li>
         </ol>
 
