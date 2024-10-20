774d370ba8185582058bda86dc42a3f780d75fe3
mr_noppo <takizawa20383461@gmail.com>
2024-10-21 07:24:53 +0900

    削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-// TODO: 更新するとISSUEが発生する問題　変更
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
