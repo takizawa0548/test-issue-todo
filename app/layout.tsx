8bae7192bfc53b8de1d820c0ebeb582e71f1eebe
mr_noppo <takizawa20383461@gmail.com>
2024-10-21 07:25:33 +0900

    テスト

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+// TODO: 更新するとISSUEが発生する問題　戻し
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
